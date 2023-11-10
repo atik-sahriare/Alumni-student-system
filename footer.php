71447a2fc2e5ecfaf2510c9ec1031895dc2ccbe7
Taiyaba063 <ttaskin162063@bscse.uiu.ac.bd>
2023-11-10 12:54:15 +0600

    initial project setup and homepage

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(0);
+session_start();
+?>
+<div class="footer animated wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="500ms">
+	<div class="container">
+			<p class="footer-class"> Welcome to our Nexus</p>	
+		</div>                                                        
+</div>	
+</body>
+</html>
